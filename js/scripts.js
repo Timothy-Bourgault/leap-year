c2fef44ba30ab6be4c4b14cc1a93b742aba1846b
Tim Bourgault <tim.bourgault@gmail.com>
2016-08-17 09:49:47 -0700

    update javascript to provide leapYear as a function creating the basic result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-$(document).ready(funtion() {
+  var leapYear = function(year) {
+    return false;
+  };
+$(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
+
   });
 });
+
+//jQuery user interface logic here
